b3e822f9791de29f42b8930332511298acffa6a2
BrBrov <epoc@list.ru>
2023-03-17 00:08:46 +0300

    feat: create header

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { SettingsComponent } from './settings/settings.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent,
+    SettingsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ youtube-client/src/app/search/search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss']
+})
+export class SearchComponent {
+  public placeholder = 'What are you want to find out?';
+}
